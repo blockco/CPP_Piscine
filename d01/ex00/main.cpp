07caa14c2d63c1a2aece29bffb530f8607552fba
robert passafaro <rpassafa@e1z3r1p24.42.us.org>
2017-07-06 21:38:20 -0700

    done with d01

--- d01/ex00/main.cpp
+++ d01/ex00/main.cpp
@@ -6,21 +6,29 @@
 /*   By: rpassafa <rpassafa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/04 16:07:04 by rpassafa          #+#    #+#             */
-/*   Updated: 2017/07/04 17:25:09 by rpassafa         ###   ########.fr       */
+/*   Updated: 2017/07/06 21:37:24 by rpassafa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Pony.hpp"
 
-int main()
+void  ponyOnTheHeap()
 {
 	Pony *h_p_1 = new Pony();
-	// Pony *h_p_0 = new Pony();
+}
 
+void  ponyOnTheStack()
+{
 	Pony s_p_1("hello", 100, 150);
-	// Pony s_p_0("bob", 500, 1000);
-
 	delete h_p_1;
+}
+
+int main()
+{
+	ponyOnTheHeap();
+	ponyOnTheStack();
+	// Pony *h_p_0 = new Pony();
+	// Pony s_p_0("bob", 500, 1000);
 	// delete h_p_0;
 	return 0;
 }
